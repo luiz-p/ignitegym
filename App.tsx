c38cfd2276362c71f64b3ad0159f8794c77d6855
Luiz Pires <lpiresjunior@gmail.com>
2022-11-03 04:28:01 -0300

    feat: config status bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 import {
   Roboto_400Regular,
@@ -10,7 +10,19 @@ export default function App () {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024'
+      }}
+    >
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   )
