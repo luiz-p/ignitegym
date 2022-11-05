88554f795dfbe7708fd154b252cdcbcef9ae5fa9
Luiz Pires <lpiresjunior@gmail.com>
2022-11-05 18:21:44 -0300

    feat: create header and filter on home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,36 @@
-import { Center, Text } from 'native-base'
+import { useState } from 'react'
+
+import { FlatList, HStack, VStack } from 'native-base'
+
+import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
 
 export function Home () {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
-    <Center flex={1}>
-      <Text color='white'>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <HStack></HStack>
+    </VStack>
   )
 }
