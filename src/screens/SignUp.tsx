bb4146ff26930c5db4af0a5635a767a77d1a8451
Luiz Pires <lpiresjunior@gmail.com>
2022-11-25 18:45:32 -0300

    feat: add types to form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,16 +7,28 @@ import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
 
+type FormDataProps = {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
+
 export function SignUp () {
   const navigation = useNavigation()
-  const { control, handleSubmit } = useForm()
+  const { control, handleSubmit } = useForm<FormDataProps>()
 
   function handleGoBack () {
     navigation.goBack()
   }
 
-  function handleSignUp (data: any) {
-    console.log(data)
+  function handleSignUp ({
+    name,
+    email,
+    password,
+    password_confirm
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
   }
 
   return (
