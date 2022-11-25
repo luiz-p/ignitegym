478447765cf65e924a5db617be14262a55dfe6df
Luiz Pires <lpiresjunior@gmail.com>
2022-11-25 20:37:43 -0300

    refactor: name and email validator

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,10 +1,12 @@
 import { Center, Heading, Image, ScrollView, Text, VStack } from 'native-base'
 import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation } from '@react-navigation/native'
 
 type FormDataProps = {
@@ -14,13 +16,20 @@ type FormDataProps = {
   password_confirm: string
 }
 
+const signUpSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+})
+
 export function SignUp () {
   const navigation = useNavigation()
   const {
     control,
     handleSubmit,
     formState: { errors }
-  } = useForm<FormDataProps>()
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  })
 
   function handleGoBack () {
     navigation.goBack()
@@ -64,7 +73,6 @@ export function SignUp () {
           <Controller
             control={control}
             name='name'
-            rules={{ required: 'Informe o nome.' }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder='Nome'
@@ -78,13 +86,6 @@ export function SignUp () {
           <Controller
             control={control}
             name='email'
-            rules={{
-              required: 'Informe o e-mail.',
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'E-mail inválido'
-              }
-            }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder='E-mail'
@@ -93,7 +94,6 @@ export function SignUp () {
                 onChangeText={onChange}
                 value={value}
                 errorMessage={errors.email?.message}
-
               />
             )}
           />
