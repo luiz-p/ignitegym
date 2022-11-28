be701675dcd0061642a86a19d5cb55d3a931f7c1
Luiz Pires <lpiresjunior@gmail.com>
2022-11-28 16:42:11 -0300

    feat: create sign out method

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,11 +2,16 @@ import { createContext, ReactNode, useEffect, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
-import { storageUserGet, storageUserSave } from '@storage/storageUser'
+import {
+  storageUserGet,
+  storageUserRemove,
+  storageUserSave
+} from '@storage/storageUser'
 
 export type AuthContextDataProps = {
   user: UserDTO
   signIn: (email: string, password: string) => Promise<void>
+  signOut: () => Promise<void>
   isLoadingUserStorageData: boolean
 }
 
@@ -35,6 +40,18 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
     }
   }
 
+  async function signOut () {
+    try {
+      setIsLoadingUserStorageData(true)
+      setUser({} as UserDTO)
+      await storageUserRemove()
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function loadUserData () {
     try {
       const userLogged = await storageUserGet()
@@ -55,7 +72,7 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }}>
+    <AuthContext.Provider value={{ user, signIn, signOut, isLoadingUserStorageData }}>
       {children}
     </AuthContext.Provider>
   )
