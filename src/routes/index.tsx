47cf2980ca00cd4241f404b9c170ea0fd7a976e3
Luiz Pires <lpiresjunior@gmail.com>
2022-11-28 16:18:04 -0300

    feat: display loading component when loading user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Box, useTheme } from 'native-base'
 
+import { Loading } from '@components/Loading'
 import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
@@ -9,10 +10,14 @@ import { AuthRoutes } from './auth.routes'
 export function Routes () {
   const { colors } = useTheme()
 
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg='gray.700'>
