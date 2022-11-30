321e3a72503df2c87cd1d0070a66110e2e8757d9
Luiz Pires <lpiresjunior@gmail.com>
2022-11-30 15:45:49 -0300

    feat: fetch exercises

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { FlatList, Heading, HStack, Text, useToast, VStack } from 'native-base'
 
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { useNavigation } from '@react-navigation/native'
+import { Loading } from '@components/Loading'
+import { ExerciseDTO } from '@dtos/ExerciseDTO'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { api } from '@services/api'
 import { AppError } from '@utils/AppErros'
 
 export function Home () {
   const [groups, setGroups] = useState<string[]>([])
-  const [exercises, setExercises] = useState([
-    'Puxada frontal',
-    'Remada curvada',
-    'Remada unilateral',
-    'Levantamento terra'
-  ])
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([])
   const [groupSelected, setGroupSelected] = useState('costas')
+  const [isLoading, setIsLoading] = useState(true)
   const toast = useToast()
 
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -44,9 +42,36 @@ export function Home () {
     }
   }
 
+  async function fetchExercisesByGroup () {
+    try {
+      setIsLoading(true)
+
+      const { data } = await api.get(`/exercises/bygroup/${groupSelected}`)
+      setExercises(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os grupos musculares.'
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   useEffect(() => {
     fetchGroups()
   }, [])
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchExercisesByGroup()
+    }, [groupSelected])
+  )
 
   return (
     <VStack flex={1}>
@@ -72,27 +97,33 @@ export function Home () {
         minH={10}
       />
 
-      <VStack flex={1} px={8}>
-        <HStack justifyContent='space-between' mb={5}>
-          <Heading color='gray.200' fontSize='md' fontFamily='heading'>
-            Exercicios
-          </Heading>
+      {isLoading
+        ? (
+        <Loading />
+          )
+        : (
+        <VStack flex={1} px={8}>
+          <HStack justifyContent='space-between' mb={5}>
+            <Heading color='gray.200' fontSize='md' fontFamily='heading'>
+              Exercicios
+            </Heading>
 
-          <Text color='gray.200' fontSize='sm'>
-            {exercises.length}
-          </Text>
-        </HStack>
+            <Text color='gray.200' fontSize='sm'>
+              {exercises.length}
+            </Text>
+          </HStack>
 
-        <FlatList
-          data={exercises}
-          keyExtractor={(item) => item}
-          renderItem={({ item }) => (
-            <ExerciseCard onPress={handleOpenExercisesDetails} />
+          <FlatList
+            data={exercises}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ExerciseCard onPress={handleOpenExercisesDetails} data={item} />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ paddingBottom: 20 }}
+          />
+        </VStack>
           )}
-          showsVerticalScrollIndicator={false}
-          _contentContainerStyle={{ paddingBottom: 20 }}
-        />
-      </VStack>
     </VStack>
   )
 }
