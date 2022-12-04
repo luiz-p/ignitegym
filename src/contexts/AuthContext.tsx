3f299ba366cd71e284a88595cd5c4d37631b887f
Luiz Pires <lpiresjunior@gmail.com>
2022-12-04 17:09:46 -0300

    feat: add token manager to api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -109,6 +109,14 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
     loadUserData()
   }, [])
 
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut)
+
+    return () => {
+      subscribe()
+    }
+  }, [signOut])
+
   return (
     <AuthContext.Provider
       value={{
