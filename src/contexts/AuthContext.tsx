a8bdfbe8c68553a08e7fa7db0cdb9ae04ed4d5ef
Luiz Pires <lpiresjunior@gmail.com>
2022-12-03 21:00:44 -0300

    feat: remove token from storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
 import {
   storageAuthTokenGet,
+  storageAuthTokenRemove,
   storageAuthTokenSave
 } from '@storage/storageAuthToken'
 import {
@@ -69,6 +70,7 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true)
       setUser({} as UserDTO)
       await storageUserRemove()
+      await storageAuthTokenRemove()
     } catch (error) {
       throw error
     } finally {
