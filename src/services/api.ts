39e64cc5ca1690c9c759068766e994ade1e24e40
Luiz Pires <lpiresjunior@gmail.com>
2022-12-04 17:19:23 -0300

    feat: check if token is invalid

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
+import { storageAuthTokenGet } from '@storage/storageAuthToken'
 import { AppError } from '@utils/AppError'
 
 type SignOut = () => void
@@ -15,11 +16,26 @@ const api = axios.create({
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (error) => {
-      if (error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message))
+    async (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expired' ||
+          requestError.response.data?.message === 'token.invalid'
+        ) {
+          const oldToken = await storageAuthTokenGet()
+
+          if (!oldToken) {
+            signOut()
+            return Promise.reject(requestError)
+          }
+        }
+        signOut()
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message))
       } else {
-        return Promise.reject(error)
+        return Promise.reject(requestError)
       }
     }
   )
