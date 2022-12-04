ea93db828a4136aebe9878b81596d31a4168985e
Luiz Pires <lpiresjunior@gmail.com>
2022-12-04 11:26:11 -0300

    feat: update user in storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -15,7 +15,8 @@ import {
 
 export type AuthContextDataProps = {
   user: UserDTO
-  signIn: (email: string, password: string) => Promise<void>
+  singIn: (email: string, password: string) => Promise<void>
+  updateUserProfile: (userUpdated: UserDTO) => Promise<void>
   signOut: () => Promise<void>
   isLoadingUserStorageData: boolean
 }
@@ -50,7 +51,7 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
     }
   }
 
-  async function signIn (email: string, password: string) {
+  async function singIn (email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password })
 
@@ -78,6 +79,15 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
     }
   }
 
+  async function updateUserProfile (userUpdated: UserDTO) {
+    try {
+      setUser(userUpdated)
+      await storageUserSave(userUpdated)
+    } catch (error) {
+      throw error
+    }
+  }
+
   async function loadUserData () {
     try {
       setIsLoadingUserStorageData(true)
@@ -101,7 +111,13 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
 
   return (
     <AuthContext.Provider
-      value={{ user, signIn, signOut, isLoadingUserStorageData }}
+      value={{
+        user,
+        singIn,
+        updateUserProfile,
+        signOut,
+        isLoadingUserStorageData
+      }}
     >
       {children}
     </AuthContext.Provider>
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -40,11 +40,11 @@ const profileSchema = yup.object({
     .string()
     .min(6, 'A senha deve ter pelo menos 6 dígitos.')
     .nullable()
-    .transform((value) => (value || null)),
+    .transform((value) => value || null),
   confirm_password: yup
     .string()
     .nullable()
-    .transform((value) => (value || null))
+    .transform((value) => value || null)
     .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
     .when('password', {
       is: (Field: any) => Field,
@@ -52,7 +52,7 @@ const profileSchema = yup.object({
         .string()
         .nullable()
         .required('Informe a confirmação da senha.')
-        .transform((value) => (value || null))
+        .transform((value) => value || null)
     })
 })
 
@@ -62,7 +62,7 @@ export function Profile () {
   const [userPhoto, setUserPhoto] = useState('https://github.com/luiz-p.png')
 
   const toast = useToast()
-  const { user } = useAuth()
+  const { user, updateUserProfile } = useAuth()
   const {
     control,
     handleSubmit,
@@ -113,7 +113,13 @@ export function Profile () {
   async function handleProfileUpdate (data: FormDataProps) {
     try {
       setIsUpdating(true)
+
+      const userUpdated = user
+      userUpdated.name = data.name
+
       await api.put('/users', data)
+
+      await updateUserProfile(userUpdated)
 
       toast.show({
         title: 'Perfil atualizado com sucesso!',
